a1627b1760f73f45242a78a35b76d33115e40f66
Connor Meth <connormeth27@gmail.com>
2020-10-14 11:26:37 -0400

    token changes

--- stocktwits.js
+++ stocktwits.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { config } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -12,4 +12,4 @@ client.on('message', message => {
 	}
 });
 
-client.login(token);
+client.login(config.token);
